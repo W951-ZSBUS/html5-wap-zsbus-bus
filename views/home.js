491f9c95dc63c243d9f8043c3474b337726ae683
w951 <w951@huahongtop.com>
2014-06-09 16:38:05 +0800

    1111
    111

---
+++ views/home.js
@@ -0,0 +1,19 @@
+﻿APP.home = function (params) {
+	var viewModel = {
+		input: {
+			line: ko.observable('')
+		},
+		searchLine: function() {
+			var line = this.input.line();
+			if (line.length == 0) {
+				DevExpress.ui.dialog.alert("请输入线路名称", dialog_title);
+			} else {
+				APP.app.navigate("line/" + this.input.line());
+			}
+		},
+		transition: function() {
+			APP.app.navigate("transition/");
+		}
+	};
+	return viewModel;
+};
